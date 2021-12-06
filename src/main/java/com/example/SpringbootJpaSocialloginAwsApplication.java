60811848a6780976e0869f4bdd75a0669ed9a874
eungsu <eungsu@me.com>
2021-12-06 09:55:06 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/SpringbootJpaSocialloginAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaSocialloginAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaSocialloginAwsApplication.class, args);
+    }
+
+}
