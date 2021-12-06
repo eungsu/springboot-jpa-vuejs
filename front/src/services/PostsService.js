9917c97944d3a02b61266e5e3a2f3145760e5fe3
eungsu <eungsu@me.com>
2021-12-06 23:56:46 +0900

    PostsService.js, board/Form.vue 컴포넌트 추가

---
+++ front/src/services/PostsService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'http://localhost/api/v1',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+});
+
+export default {
+  save(posts) {
+    return http.post('/posts', posts);
+  }
+}
--- front/src/views/board/Form.vue
+++ front/src/views/board/Form.vue
@@ -5,6 +5,13 @@
         <h1 class="fs-5 border p-3 shadow-sm">게시글 등록폼</h1>
       </div>
     </div>
+    <div v-if="isFail" class="row mb-2">
+      <div class="col">
+        <div class="alert alert-danger" role="alert">
+          게시글 등록 중 오류가 발생하였습니다.
+        </div>
+      </div>
+    </div>
     <div class="row mb-3">
       <div class="col">
         <div class="border p-3 bg-light shadow-sm">
@@ -13,15 +20,15 @@
             <input type="text" class="form-control" id="title" v-model="title" placeholder="제목을 입력하세요"/>
           </div>
           <div class="mb-3">
-            <label for="writer" class="form-label">작성자</label>
-            <input type="text" class="form-control" id="writer" v-model="writer" placeholder="작성자를 입력하세요"/>
+            <label for="author" class="form-label">작성자</label>
+            <input type="text" class="form-control" id="author" v-model="author" placeholder="작성자를 입력하세요"/>
           </div>
           <div class="mb-3">
             <label for="content" class="form-label">내용</label>
             <textarea class="form-control" rows="5" id="content" v-model="content" placeholder="내용을 입력하세요"></textarea>
           </div>
           <div class="mb-3 text-end">
-            <button type="button" class="btn btn-primary">등록</button>
+            <button type="button" class="btn btn-primary" :disabled="!canSubmit" @click="save">등록</button>
           </div>
         </div>
       </div>
@@ -30,13 +37,40 @@
 </template>
 
 <script>
+import PostsService from '../../services/PostsService'
+
 export default {
   name: "BoardForm",
   data() {
     return {
+      isFail: false,
       title: "",
-      writer: "",
+      author: "",
       content: ""
+    }
+  },
+  methods: {
+    clearField: function() {
+      this.title = "";
+      this.author = "";
+      this.content = "";
+    },
+    save: function() {
+      var posts = {title: this.title, author: this.author, content: this.content};
+      PostsService.save(posts)
+        .then((response) => {
+          if (response.data.success) {
+            this.$router.push('/');
+          } else {
+            this.isFail = true;
+            this.clearField();
+          }
+        })
+    }
+  },
+  computed: {
+    canSubmit() {
+      return this.title && this.author && this.content;
     }
   }
 }
