8ea95b31ec840d1bf322136d697849a5934d9b9b
eungsu <eungsu@me.com>
2021-12-06 12:54:16 +0900

    PostsDto에 생성시간, 수정시간 추가

--- src/main/java/com/example/dto/PostsDto.java
+++ src/main/java/com/example/dto/PostsDto.java
@@ -3,17 +3,23 @@ package com.example.dto;
 import com.example.domain.posts.Posts;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostsDto {
     private Long id;
     private String title;
     private String content;
     private String author;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
 
     public PostsDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
+        this.createdDate = entity.getCreatedDate();
+        this.modifiedDate = entity.getModifiedDate();
     }
 }
