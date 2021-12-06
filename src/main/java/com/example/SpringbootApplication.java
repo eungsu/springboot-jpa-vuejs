eacc92429b83beef3394742bdd946fae8953ead7
eungsu <eungsu@me.com>
2021-12-06 12:50:00 +0900

    JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class SpringbootJpaSocialloginAwsApplication {
+public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootJpaSocialloginAwsApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 
 }
