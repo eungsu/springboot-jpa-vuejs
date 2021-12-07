89a015512baa21ae05bec6b97b258e4fd07060d1
eungsu <eungsu@me.com>
2021-12-07 11:53:26 +0900

    LocalDateTime의 jsonformat 변경

--- src/main/java/com/example/dto/PostsDto.java
+++ src/main/java/com/example/dto/PostsDto.java
@@ -1,6 +1,8 @@
 package com.example.dto;
 
 import com.example.entity.posts.Posts;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -11,7 +13,9 @@ public class PostsDto {
     private String title;
     private String content;
     private String author;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifiedDate;
 
     public PostsDto(Posts entity) {
